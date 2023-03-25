7610a1f6adfc5efb990e2c0bc1b64f4e0da74f64
Rouan_Laporal <rouan.laporal@outlook.com>
2023-03-25 21:17:50 +0100

    Chores update square and board function

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import Square from './components/Square/Square';
 
 const Board = () => {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
     </>
   );
